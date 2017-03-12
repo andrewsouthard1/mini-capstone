190f3b128d5fafda5896de8655d9fadfd6a77e7e
Andrew <andrewsouthard111@gmail.com>
2017-03-12 09:48:17 -0400

    Add form to new page

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -4,15 +4,19 @@ class ComicsController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    render 'create.html.erb'
+  end
+
   def show
     comic_id = params[:id]
     p "*******"
     p comic_id
     @comic = Product.find_by(id: comic_id)
     render 'show.html.erb'
-  end
-
-  def create
-    render 'create.html.erb'
   end
 end
