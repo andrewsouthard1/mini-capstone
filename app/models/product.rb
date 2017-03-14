5c4f086c7730aa95508baf6d631b3e08844af751
Andrew <andrewsouthard111@gmail.com>
2017-03-14 15:31:24 -0400

    Price type in db changed to decimal, added under_2 model method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  def under_2
+    output_message = ""
+    output_message += "Discount Item" if price < 2.0
+    output_message += "Everday Value!!" if price >= 2.0
+    output_message
+  end
 end
